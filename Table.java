3f9ca9cfae04d45e8e3e711cc23e39ed1039f98c
TOP-GUN66 <898803540@qq.com>
2017-09-09 00:51:57 +0800

    ch1_exercise 9*9Table
    九九乘法表

---
+++ Table.java
@@ -0,0 +1,23 @@
+package ex_ch1;
+
+class Table {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("9*9³Ë·¨±í");  
+         
+	         
+	        int x,y;  
+	  
+	        for(x = 0;x <= 9; x++)  
+	        {  
+	            for(y = x;y <= 9; y++)  
+	            {  
+	                System.out.print(y+"*"+x+"="+x*y+"\t");  
+	            }  
+	            System.out.println();  
+	}
+	}
+	}
+
